aaa9a0281c9a8d5f8bb9aaccca0b8ee8fee23e9a
Thomas Texier <sharkone@en-mousse.org>
2014-03-27 11:13:34 -0400

    Parse http-port and inactivity-timeout arguments as integers

--- cherrytorrent.py
+++ cherrytorrent.py
@@ -8,13 +8,13 @@ from cherrytorrent import server
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('uri', help='Magnet link or torrent file URL')
-    arg_parser.add_argument('-p', '--http-port', default=8080, help='Port used for HTTP server')
+    arg_parser.add_argument('-p', '--http-port', type=int, default=8080, help='Port used for HTTP server')
     arg_parser.add_argument('-d', '--download-dir', default='.', help='Directory to use for downloading')
-    arg_parser.add_argument('-t', '--inactivity-timeout', default=30, help='Inactivity timeout')
+    arg_parser.add_argument('-t', '--inactivity-timeout', type=int, default=30, help='Inactivity timeout')
     arg_parser.add_argument('-k', '--keep-files', default=False, help='Keep downloaded files upon stopping')
     args = arg_parser.parse_args()
 
-    server = cherrytorrent.server.Server(int(args.http_port), args.inactivity_timeout, args.uri, args.download_dir, args.keep_files)
+    server = cherrytorrent.server.Server(args.http_port, args.inactivity_timeout, args.uri, args.download_dir, args.keep_files)
     server.run()
 
 ################################################################################
