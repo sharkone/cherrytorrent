59d41537a75c645756a719f078e80d084d230351
Thomas Texier <sharkone@en-mousse.org>
2014-03-27 14:27:05 -0400

    Added torrent encryption

--- cherrytorrent.py
+++ cherrytorrent.py
@@ -14,6 +14,7 @@ def main():
     arg_parser.add_argument('-th',  '--torrent-high-port', type=int, default=6999, help='Higher bound of BitTorrent session port range')
     arg_parser.add_argument('-tdl', '--torrent-download-rate', type=int, default=0, help='Maximum download rate in kB/s (0: Unlimited)')
     arg_parser.add_argument('-tul', '--torrent-upload-rate', type=int, default=0, help='Maximum upload rate in kB/s (0: Unlimited)')
+    arg_parser.add_argument('-te',  '--torrent-encryption', default=1, help='Encryption: 0: Forced 1: Enabled (default) 2:Disabled)')
     arg_parser.add_argument('-td',  '--torrent-download-dir', default='.', help='Directory to use for downloading')
     arg_parser.add_argument('-tk',  '--torrent-keep-files', default=False, help='Keep downloaded files upon stopping')
     args = arg_parser.parse_args()
@@ -29,6 +30,7 @@ def main():
                         'high_port':        args.torrent_high_port,
                         'download_rate':    args.torrent_download_rate,
                         'upload_rate':      args.torrent_upload_rate,
+                        'encryption':       args.torrent_encryption,
                         'download_dir':     args.torrent_download_dir,
                         'keep_files':       args.torrent_keep_files
                      }
--- cherrytorrent/downloader.py
+++ cherrytorrent/downloader.py
@@ -22,26 +22,35 @@ class DownloaderPlugin(cherrypy.process.plugins.SimplePlugin):
         self.bus.log('[Downloader] Listening on {0}:{1}'.format(self.torrent_config['low_port'], self.torrent_config['high_port']))
         self.session.listen_on(self.torrent_config['low_port'], self.torrent_config['high_port'])
 
-        self.bus.log('[Downloader] Setting session settings')
-        self.session.settings().announce_to_all_tiers = True
-        self.session.settings().announce_to_all_trackers = True
-        self.session.settings().connection_speed = 100
-        self.session.settings().peer_connect_timeout = 2
-        self.session.settings().rate_limit_ip_overhead = True
-        self.session.settings().request_timeout = 5
-        self.session.settings().torrent_connect_boost = 100
+        self.bus.log('[Downloader] Applying session settings')
+        session_settings = self.session.settings()
+        session_settings.announce_to_all_tiers = True
+        session_settings.announce_to_all_trackers = True
+        session_settings.connection_speed = 100
+        session_settings.peer_connect_timeout = 2
+        session_settings.rate_limit_ip_overhead = True
+        session_settings.request_timeout = 5
+        session_settings.torrent_connect_boost = 100
 
         if self.torrent_config['download_rate'] > 0:
-            self.session.settings().download_rate_limit = self.torrent_config['download_rate'] * 1024
+            session_settings.download_rate_limit = self.torrent_config['download_rate'] * 1024
         if self.torrent_config['upload_rate'] > 0:
-            self.session.settings().upload_rate_limit = self.torrent_config['upload_rate'] * 1024
+            session_settings.upload_rate_limit = self.torrent_config['upload_rate'] * 1024
+        self.session.set_settings(session_settings)
+
+        self.bus.log('[Downloader] Applying encryption settings')
+        encryption_settings = libtorrent.pe_settings()
+        encryption_settings.out_enc_policy = libtorrent.enc_policy(self.torrent_config['encryption'])
+        encryption_settings.in_enc_policy = libtorrent.enc_policy(self.torrent_config['encryption'])
+        encryption_settings.allowed_enc_level = libtorrent.enc_level.both
+        encryption_settings.prefer_rc4 = True
+        self.session.set_pe_settings(encryption_settings)
 
         self.bus.log('[Downloader] Adding requested torrent')
         add_torrent_params                 = {}
         add_torrent_params['url']          = self.torrent_config['uri']
         add_torrent_params['save_path']    = self.torrent_config['download_dir']
         add_torrent_params['storage_mode'] = libtorrent.storage_mode_t.storage_mode_sparse
-
         self.torrent_handle = self.session.add_torrent(add_torrent_params)
         self.torrent_handle.set_sequential_download(True)
 
