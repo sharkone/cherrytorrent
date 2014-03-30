2b96c82c576fd662ff705764d7131db6ff8d6fb4
Thomas Texier <sharkone@en-mousse.org>
2014-03-30 00:07:45 -0400

    Improved help

--- cherrytorrent.py
+++ cherrytorrent.py
@@ -6,15 +6,15 @@ from cherrytorrent import server
 
 ################################################################################
 def main():
-    arg_parser = argparse.ArgumentParser()
+    arg_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     arg_parser.add_argument('torrent_uri', help='Magnet link or torrent file URL')
     arg_parser.add_argument('-hp',  '--http-port', type=int, default=8080, help='Port used for HTTP server')
     arg_parser.add_argument('-ht',  '--http-inactivity-timeout', type=int, default=30, help='Inactivity timeout')
     arg_parser.add_argument('-tl',  '--torrent-low-port', type=int, default=6900, help='Lower bound of BitTorrent session port range')
     arg_parser.add_argument('-th',  '--torrent-high-port', type=int, default=6999, help='Higher bound of BitTorrent session port range')
-    arg_parser.add_argument('-tdl', '--torrent-download-rate', type=int, default=0, help='Maximum download rate in kB/s (0: Unlimited)')
-    arg_parser.add_argument('-tul', '--torrent-upload-rate', type=int, default=0, help='Maximum upload rate in kB/s (0: Unlimited)')
-    arg_parser.add_argument('-te',  '--torrent-encryption', default=1, help='Encryption: 0: Forced 1: Enabled (default) 2:Disabled)')
+    arg_parser.add_argument('-tdl', '--torrent-download-rate', type=int, default=0, help='Maximum download rate in kB/s, 0 = Unlimited')
+    arg_parser.add_argument('-tul', '--torrent-upload-rate', type=int, default=0, help='Maximum upload rate in kB/s, 0 = Unlimited')
+    arg_parser.add_argument('-te',  '--torrent-encryption', default=1, help='Encryption mode, 0: Forced, 1: Enabled, 2: Disabled')
     arg_parser.add_argument('-td',  '--torrent-download-dir', default='.', help='Directory to use for downloading')
     arg_parser.add_argument('-tk',  '--torrent-keep-files', dest='torrent_keep_files', action='store_true', help='Keep downloaded files upon stopping')
     args = arg_parser.parse_args()
