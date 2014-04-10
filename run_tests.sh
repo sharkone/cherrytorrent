50c163bc98d2fe0e2db3a49ec6601845f955e4c4
Thomas Texier <sharkone@en-mousse.org>
2014-04-10 18:20:34 -0400

    Integrated Travis CI

---
+++ run_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+python cherrytorrent.py "magnet:?xt=urn:btih:c39fe3eefbdb62da9c27eb6398ff4a7d2e26e7ab&dn=big+buck+bunny+bdrip+xvid+medic&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337"
