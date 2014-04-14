02218ef7612b8499c294559706aae075aea55a4d
Thomas Texier <sharkone@en-mousse.org>
2014-04-14 11:29:56 -0400

    No longer log to file automatically but rather log customization

--- cherrytorrent.py
+++ cherrytorrent.py
@@ -1,12 +1,12 @@
 ################################################################################
 import argparse
 import cherrytorrent
+import logging
 
 ################################################################################
 def main():
     arg_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     arg_parser.add_argument('-hp',  '--http-port', type=int, default=8080, help='Port used for HTTP server')
-    arg_parser.add_argument('-hl',  '--http-log-dir', default='.', help='Log file destination directory')
     arg_parser.add_argument('-tp',  '--torrent-port', type=int, default=6900, help='Port used for BitTorrent incoming connections')
     arg_parser.add_argument('-tdl', '--torrent-download-rate', type=int, default=0, help='Maximum download rate in kB/s, 0 = Unlimited')
     arg_parser.add_argument('-tul', '--torrent-upload-rate', type=int, default=0, help='Maximum upload rate in kB/s, 0 = Unlimited')
@@ -15,7 +15,6 @@ def main():
 
     http_config    = {
                         'port':     args.http_port,
-                        'log_dir':  args.http_log_dir,
                      }
 
     torrent_config = {
